b4f7b005ed4a5ad8a0fea4fbfbd40f67cbd25682
Aasha98 <aasha3388@gmail.com>
2019-12-11 13:42:08 +0530

    Initial Commit

--- db/migrate/20191209050931_add_bank_acc_num_to_salaries.rb
+++ db/migrate/20191209050931_add_bank_acc_num_to_salaries.rb
@@ -5,7 +5,7 @@ class AddBankAccNumToSalaries < ActiveRecord::Migration[6.0]
     add_column :salaries, :ifsc_code, :string
     add_column :salaries, :account_holder_name, :string
     add_column :salaries, :pan_number, :string
-    add_column :salaries, :no_of_Working_days, :sring
+    add_column :salaries, :no_of_Working_days, :string
     add_column :salaries, :employee_id, :string
   end
 end
